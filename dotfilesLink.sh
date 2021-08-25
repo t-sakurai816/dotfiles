425f7f457e08a3c4f7e2602cb3250a912eff118a
t-sakurai816 <t.devsaku@gmail.com>
2021-08-25 23:09:30 +0900

    ADD: ashrc

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -10,6 +10,9 @@ ln -s ~/dotfiles/vim/* ~/.vim
 if [ "`echo $SHELL | grep 'fish'`" ]; then
   echo "containing fish"
   ln -sf ~/dotfiles/config.fish ~/.config/fish/config.fish
+else if [ "`echo $SHELL | grep 'ash'`" ]; then
+  echo "containing ash"
+  ls -sf ~/dotfiles/ashrc ~/.ashrc
 else
   echo "not contain fish"
 fi
